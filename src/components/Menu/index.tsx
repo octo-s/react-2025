db628b618f28687fb7a43741a54b01c8c911fd6f
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-06-27 19:23:25 +0200

    add menu link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,27 +1,19 @@
 import React from "react";
 import { type TRestaurant } from "../../types/restaurant";
 import sharedStyles from "../../styles/shared.module.scss";
-import { Link } from "react-router";
 import styles from "./Menu.module.scss";
-import { selectDishEntities } from "../../redux/entities/dish/dishSlice";
-import { useSelector } from "react-redux";
+import { MenuItemLink } from "../MenuItemLink";
 
 type MenuProps = {
   menu: TRestaurant["menu"];
 };
 
 export const Menu: React.FC<MenuProps> = ({ menu }) => {
-  const restaurantDishes = useSelector(selectDishEntities);
-
   return menu.length ? (
     <div className={styles.menuList}>
-      {menu.map((id) => {
-        return (
-          <Link key={id} to={`/dish/${id}`} className={styles.menuItem}>
-            {restaurantDishes[id]?.name}
-          </Link>
-        );
-      })}
+      {menu.map((id) => (
+        <MenuItemLink key={id} id={id} />
+      ))}
     </div>
   ) : (
     <div className={sharedStyles.empty}>Ресторан пока не добавил блюда</div>
